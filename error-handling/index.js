86687f88aa9d901cfd4a3ef30a7da67912b1f5ce
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-06 12:06:53 +0200

    correcting error handling

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,17 +1,17 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
-    // this middleware runs whenever requested page is not available
-    res.status(404).render("not-found");
-  });
+    // this middleware runs whenever requested resource is not available
+    res.status(404).json({ message: 'Resource not found' })
+  })
 
   app.use((err, req, res, next) => {
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
-    console.error("ERROR", req.method, req.path, err);
+    console.error('ERROR', req.method, req.path, err)
 
-    // only render if the error ocurred before sending the response
+    // only send response if the response has not yet been sent
     if (!res.headersSent) {
-      res.status(500).render("error");
+      res.status(500).json({ message: 'Internal server error' })
     }
-  });
-};
+  })
+}
